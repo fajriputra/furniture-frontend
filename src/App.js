4811606247d9b1b045d2593e4b9cdea27c3938b5
AL FAJRI PUTRA PRATAMA <fajriwn27@gmail.com>
2021-06-27 12:21:56 +0700

    update host server

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "assets/scss/styles.scss";
 function App() {
   return (
     <div className="App">
-      <h1 className="text-center text-gray-500">HELLO REACT !</h1>
+      <h1 className="text-center text-gray-300">HELLO REACT !</h1>
     </div>
   );
 }
